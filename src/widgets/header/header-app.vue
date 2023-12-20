7d6ec5f1bf61bf800bd3e2a6d922c3ae055b1ad6
timon <timonsus@gmail.com>
2023-12-20 16:16:22 +0500

    headerstyles

--- src/widgets/header/header-app.vue
+++ src/widgets/header/header-app.vue
@@ -4,8 +4,8 @@
 <template>
   <header class="header">
     <nav class="nav">
-      <RouterLink to="/">Меню</RouterLink>
-      <RouterLink to="/add-category">Добавить категорию</RouterLink>
+      <RouterLink class="header__nav-link" to="/">Меню</RouterLink>
+      <RouterLink class="header__nav-link" to="/add-category">Добавить категорию</RouterLink>
     </nav>
   </header>
 </template>
@@ -13,12 +13,24 @@
 <style scoped lang="scss">
 .header {
   padding: 1em;
-  background-color: var(--main-color);
+  background-color: var(--background-color);
+  box-shadow: 0 5px 5px rgba($color: #000000, $alpha: 0.2);
 
   .nav {
     display: flex;
     align-items: center;
     gap: 1em;
+  }
+
+  .header__nav-link {
+    font-weight: 600;
+    color: rgb(250, 200, 150);
+    text-decoration: none;
+    
+    &:hover, &.router-link-active {
+      color: var(--main-color);
+      border-bottom: solid 2px var(--main-color);
+    }
   }
 }
 </style>
